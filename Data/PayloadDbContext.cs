f3c8d239b6d261f1ce7f8464d0175495b4a224c3
xrjohn <704913476@qq.com>
2021-02-09 14:02:47 +0800

    Init

---
+++ Data/PayloadDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeployWeb.Data
+{
+    public class PayloadDbContext : DbContext
+    {
+        public PayloadDbContext(DbContextOptions<PayloadDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Services/GithubSvController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DeployWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DeployWeb.Services
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GithubSvController : ControllerBase
+    {
+        [HttpPost]
+        public void Receiver(string para)
+        {
+            var model = JsonConvert.DeserializeObject<PayloadModel>(para);
+            SavePushItems(model);
+        }
+
+        private void SavePushItems(PayloadModel model)
+        {
+            //TODO:
+        }
+    }
+}
